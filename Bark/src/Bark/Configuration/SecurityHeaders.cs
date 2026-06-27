39f8843154a7bb12e975d06dc63b7bed6603806a
hawkinslabdev <59891413+hawkinslabdev@users.noreply.github.com>
2026-06-27 18:52:15 +0200

    feat: add UseSecurityHeaders extension for custom CSP

--- Bark/src/Bark/Configuration/SecurityHeaders.cs
+++ Bark/src/Bark/Configuration/SecurityHeaders.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+
 namespace Bark.Configuration;
 
 public static class SecurityHeaders
 {
-    private const string ContentSecurityPolicy =
+    public const string DefaultCsp =
         "default-src 'self'; " +
         "script-src 'self' 'unsafe-inline'; " +
         "style-src 'self' 'unsafe-inline'; " +
@@ -11,13 +13,25 @@ public static class SecurityHeaders
         "connect-src 'self'; " +
         "frame-ancestors 'none'";
 
-    public static Task Apply(HttpContext context, Func<Task> next)
+    public static Task Apply(HttpContext context, Func<Task> next) =>
+        Apply(context, next, DefaultCsp);
+
+    public static Task Apply(HttpContext context, Func<Task> next, string contentSecurityPolicy)
     {
         context.Response.Headers.XContentTypeOptions = "nosniff";
         context.Response.Headers.XFrameOptions = "DENY";
         context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
-        context.Response.Headers.ContentSecurityPolicy = ContentSecurityPolicy;
-        
+        context.Response.Headers.ContentSecurityPolicy = contentSecurityPolicy;
+
         return next();
+    }
+}
+
+public static class SecurityHeadersExtensions
+{
+    public static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder app, string? contentSecurityPolicy = null)
+    {
+        var csp = contentSecurityPolicy ?? SecurityHeaders.DefaultCsp;
+        return app.Use((context, next) => SecurityHeaders.Apply(context, next, csp));
     }
 }
